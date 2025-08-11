ce532ba0674ab0126b34d7b779a38c3b031166af
Ceejay Santos <7078ceejay@gmail.com>
2025-08-11 14:52:17 +0800

    setup

---
+++ backend/api/urls.py
@@ -0,0 +1,15 @@
+
+from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+from .Views.user_views import registerUser, test
+
+urlpatterns = [
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('register/', registerUser, name='register_user'),
+    path('test/', test, name='test'),
+    
+]
